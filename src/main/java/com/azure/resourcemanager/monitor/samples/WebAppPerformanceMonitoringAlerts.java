c5a8f4f061485694d0ef7cd4b52ef1b3f852d0a4
Azure-Fluent <azfluent@microsoft.com>
2020-10-26 13:10:28 +0800

    Release azure-resourcemanager 2.0.0

--- src/main/java/com/azure/resourcemanager/monitor/samples/WebAppPerformanceMonitoringAlerts.java
+++ src/main/java/com/azure/resourcemanager/monitor/samples/WebAppPerformanceMonitoringAlerts.java
@@ -1,25 +1,25 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-package com.microsoft.azure.management.monitor.samples;
+package com.azure.resourcemanager.monitor.samples;
 
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.appservice.AppServicePlan;
-import com.microsoft.azure.management.appservice.OperatingSystem;
-import com.microsoft.azure.management.appservice.PricingTier;
-import com.microsoft.azure.management.monitor.ActionGroup;
-import com.microsoft.azure.management.monitor.MetricAlert;
-import com.microsoft.azure.management.monitor.MetricAlertRuleCondition;
-import com.microsoft.azure.management.monitor.MetricAlertRuleTimeAggregation;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
-import com.microsoft.azure.management.samples.Utils;
-import com.microsoft.rest.LogLevel;
-import org.joda.time.Period;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.appservice.models.AppServicePlan;
+import com.azure.resourcemanager.appservice.models.OperatingSystem;
+import com.azure.resourcemanager.appservice.models.PricingTier;
+import com.azure.resourcemanager.monitor.models.ActionGroup;
+import com.azure.resourcemanager.monitor.models.MetricAlert;
+import com.azure.resourcemanager.monitor.models.MetricAlertRuleCondition;
+import com.azure.resourcemanager.monitor.models.MetricAlertRuleTimeAggregation;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.resourcemanager.samples.Utils;
 
-import java.io.File;
+import java.time.Duration;
 
 /**
  * This sample shows examples of configuring Metric Alerts for WebApp instance performance monitoring through app service plan.
@@ -32,19 +32,19 @@ public final class WebAppPerformanceMonitoringAlerts {
 
     /**
      * Main function which runs the actual sample.
-     * @param azure instance of the azure client
+     * @param azureResourceManager instance of the azure client
      * @return true if sample runs successfully
      */
-    public static boolean runSample(Azure azure) {
-        final String rgName = Utils.createRandomName("rgMonitor");
+    public static boolean runSample(AzureResourceManager azureResourceManager) {
+        final String rgName = Utils.randomResourceName(azureResourceManager, "rgMonitor", 20);
 
         try {
             // ============================================================
             // Create an App Service plan
             System.out.println("Creating App Service plan");
 
-            AppServicePlan servicePlan = azure.appServices().appServicePlans().define("HighlyAvailableWebApps")
-                    .withRegion(Region.US_EAST2)
+            AppServicePlan servicePlan = azureResourceManager.appServicePlans().define("HighlyAvailableWebApps")
+                    .withRegion(Region.US_SOUTH_CENTRAL)
                     .withNewResourceGroup(rgName)
                     .withPricingTier(PricingTier.PREMIUM_P1)
                     .withOperatingSystem(OperatingSystem.WINDOWS)
@@ -55,7 +55,7 @@ public final class WebAppPerformanceMonitoringAlerts {
 
             // ============================================================
             // Create an action group to send notifications in case metric alert condition will be triggered
-            ActionGroup ag = azure.actionGroups().define("criticalPerformanceActionGroup")
+            ActionGroup ag = azureResourceManager.actionGroups().define("criticalPerformanceActionGroup")
                     .withExistingResourceGroup(rgName)
                     .defineReceiver("tierOne")
                         .withPushNotification("ops_on_duty@performancemonitoring.com")
@@ -72,12 +72,12 @@ public final class WebAppPerformanceMonitoringAlerts {
 
             // ============================================================
             // Set a trigger to fire each time
-            MetricAlert ma = azure.alertRules().metricAlerts().define("Critical performance alert")
+            MetricAlert ma = azureResourceManager.alertRules().metricAlerts().define("Critical performance alert")
                     .withExistingResourceGroup(rgName)
                     .withTargetResource(servicePlan.id())
-                    .withPeriod(Period.minutes(5))
-                    .withFrequency(Period.minutes(1))
-                    .withAlertDetails(3, "This alert rule is for U5 – Single resource-multiple criteria – with dimensions – with star")
+                    .withPeriod(Duration.ofMinutes(5))
+                    .withFrequency(Duration.ofMinutes(1))
+                    .withAlertDetails(3, "This alert rule is for U5 - Single resource-multiple criteria - with dimensions - with star")
                     .withActionGroups(ag.id())
                     .defineAlertCriteria("Metric1")
                         .withMetricName("CPUPercentage", "Microsoft.Web/serverfarms")
@@ -88,19 +88,15 @@ public final class WebAppPerformanceMonitoringAlerts {
             Utils.print(ma);
 
             return true;
-        } catch (Exception f) {
-            System.out.println(f.getMessage());
-            f.printStackTrace();
         } finally {
-            if (azure.resourceGroups().getByName(rgName) != null) {
+            if (azureResourceManager.resourceGroups().getByName(rgName) != null) {
                 System.out.println("Deleting Resource Group: " + rgName);
-                azure.resourceGroups().deleteByName(rgName);
+                azureResourceManager.resourceGroups().beginDeleteByName(rgName);
                 System.out.println("Deleted Resource Group: " + rgName);
             } else {
                 System.out.println("Did not create any resources in Azure. No clean up is necessary");
             }
         }
-        return false;
     }
 
     /**
@@ -110,17 +106,21 @@ public final class WebAppPerformanceMonitoringAlerts {
     public static void main(String[] args) {
         try {
 
-            final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+            final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                .build();
 
-            Azure azure = Azure.configure()
-                    .withLogLevel(LogLevel.NONE)
-                    .authenticate(credFile)
-                    .withDefaultSubscription();
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                .configure()
+                .withLogLevel(HttpLogDetailLevel.BASIC)
+                .authenticate(credential, profile)
+                .withDefaultSubscription();
 
             // Print selected subscription
-            System.out.println("Selected subscription: " + azure.subscriptionId());
+            System.out.println("Selected subscription: " + azureResourceManager.subscriptionId());
 
-            runSample(azure);
+            runSample(azureResourceManager);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
